b333edbbe53118928864c231524842c43c1142db
Akhil Shaju <akhilakku1030@gmail.com>
2026-02-05 16:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,126 +1,149 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 import torch
 import librosa
 import numpy as np
 import os
 import webbrowser
 import torch.nn.functional as F
-
+from scipy.signal import butter,lfilter
 from models.custom import custommodel
-from config import sample_rate, n_mels
+from config import sample_rate,n_mels
 
+app=Flask(__name__)
 
-app = Flask(__name__)
+DEVICE=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:",DEVICE)
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Using device:", DEVICE)
-
-model = custommodel(num_classes=5).to(DEVICE)
+model=custommodel(num_classes=5).to(DEVICE)
 
 model.load_state_dict(
     torch.load(
-        "checkpoints/best_model1.pth",
+        "checkpoints/best_model11.pth",
         map_location=DEVICE,
         weights_only=True
     )
 )
 
 model.eval()
-CLASSES = [
-    "COPD",            
-    "Healthy",         
-    "URTI",            
-    "Bronchiectasis",  
-    "Pneumonia"        
+
+CLASSES=[
+    "COPD",
+    "Healthy",
+    "URTI",
+    "Bronchiectasis",
+    "Pneumonia"
 ]
-def extract_features(y, sr):
-    mel = librosa.feature.melspectrogram(
+
+def bandpass_filter(y,sr,low=100,high=2000):
+    nyq=0.5*sr
+    low=low/nyq
+    high=high/nyq
+    b,a=butter(5,[low,high],btype="band")
+    return lfilter(b,a,y)
+
+def preprocess_signal(y,sr):
+    y=librosa.effects.preemphasis(y)
+    y=bandpass_filter(y,sr)
+    return y
+
+def extract_features(y,sr):
+
+    stft=np.abs(librosa.stft(y))
+    stft_db=librosa.amplitude_to_db(stft,ref=np.max)
+
+    mel=librosa.feature.melspectrogram(
         y=y,
         sr=sr,
         n_mels=n_mels
     )
 
-    logmel = librosa.power_to_db(mel, ref=np.max)
-    mfcc = librosa.feature.mfcc(
+    logmel=librosa.power_to_db(mel,ref=np.max)
+
+    mfcc=librosa.feature.mfcc(
         y=y,
         sr=sr,
         n_mfcc=n_mels
     )
-    feature = np.stack([logmel, mfcc], axis=0)
+
+    chroma=librosa.feature.chroma_stft(
+        y=y,
+        sr=sr
+    )
+
+    target_freq=n_mels
+    target_time=logmel.shape[1]
+
+    mfcc=librosa.util.fix_length(mfcc,size=target_time,axis=1)
+    chroma=librosa.util.fix_length(chroma,size=target_time,axis=1)
+    stft_db=librosa.util.fix_length(stft_db,size=target_time,axis=1)
+
+    chroma=librosa.util.fix_length(chroma,size=target_freq,axis=0)
+    mfcc=mfcc[:target_freq,:]
+    stft_db=stft_db[:target_freq,:]
+
+    feature=np.stack(
+        [logmel,mfcc,chroma,stft_db],
+        axis=0
+    )
 
     return feature
 
-def pad_or_crop(spec, max_len=300):
-    if spec.dim() == 4:
-
-        b, c, h, t = spec.shape
-
-        if t < max_len:
-            pad = max_len - t
-            spec = F.pad(spec, (0, pad))
-        else:
-            spec = spec[:, :, :, :max_len]
-
-    elif spec.dim() == 3:
-
-        c, h, t = spec.shape
-
-        if t < max_len:
-            pad = max_len - t
-            spec = F.pad(spec, (0, pad))
-        else:
-            spec = spec[:, :, :max_len]
-
-    return spec
-
 def preprocess_audio(file_path):
 
-    y, sr = librosa.load(file_path, sr=sample_rate)
-    feature = extract_features(y, sr)
-    feature = torch.tensor(
+    y,sr=librosa.load(file_path,sr=sample_rate)
+
+    y=preprocess_signal(y,sr)
+
+    feature=extract_features(y,sr)
+
+    feature=torch.tensor(
         feature,
         dtype=torch.float32
-    ).unsqueeze(0) 
-    feature = pad_or_crop(feature, max_len=300)
+    ).unsqueeze(0)
+
+    if feature.shape[-1]<300:
+        pad=300-feature.shape[-1]
+        feature=F.pad(feature,(0,pad))
+    else:
+        feature=feature[:,:,:,:300]
 
     return feature.to(DEVICE)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
 
-    prediction = None
-    confidence = None
-    probs_list = None
+    prediction=None
+    confidence=None
+    probs_list=None
 
+    if request.method=="POST":
 
-    if request.method == "POST":
-
-        file = request.files.get("audio")
+        file=request.files.get("audio")
 
         if file:
-            upload_dir = "uploads"
-            os.makedirs(upload_dir, exist_ok=True)
 
-            file_path = os.path.join(upload_dir, file.filename)
+            upload_dir="uploads"
+            os.makedirs(upload_dir,exist_ok=True)
+
+            file_path=os.path.join(upload_dir,file.filename)
 
             file.save(file_path)
 
-            x = preprocess_audio(file_path)
+            x=preprocess_audio(file_path)
 
             with torch.no_grad():
 
-                out = model(x)
+                out=model(x)
 
-                probs = torch.softmax(out, dim=1)
+                probs=torch.softmax(out,dim=1)
 
-                pred = torch.argmax(probs, dim=1).item()
+                pred=torch.argmax(probs,dim=1).item()
 
-                prediction = CLASSES[pred]
+                prediction=CLASSES[pred]
 
-                confidence = probs[0][pred].item() * 100
+                confidence=probs[0][pred].item()*100
 
-                probs_list = probs[0].cpu().numpy().tolist()
-
+                probs_list=probs[0].cpu().numpy().tolist()
 
     return render_template(
         "index.html",
@@ -130,7 +153,7 @@ def index():
         classes=CLASSES
     )
 
-if __name__ == "__main__":
+if __name__=="__main__":
 
     webbrowser.open("http://127.0.0.1:5000")
 
