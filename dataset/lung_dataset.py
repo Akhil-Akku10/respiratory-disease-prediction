36cb5955cbbe218ebfbfa4a3d01c2bcc30ca23e9
Akhil Shaju <akhilakku1030@gmail.com>
2026-02-05 16:49:04 +0530

    Update lung_dataset.py

--- dataset/lung_dataset.py
+++ dataset/lung_dataset.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 import numpy as np
 from collections import Counter
 import torch.nn.functional as F
+from scipy.signal import butter, lfilter
 from config import sample_rate, n_mels, processed_path
 CLASS_NAMES = [
     "Copd",
@@ -16,7 +17,19 @@ CLASS_NAMES = [
     "Pneumonia"
 ]
 CLASS_TO_IDX = {cls: i for i, cls in enumerate(CLASS_NAMES)}
+def bandpass_filter(y, sr, low=100, high=2000):
+    nyq = 0.5 * sr
+    low = low / nyq
+    high = high / nyq
+    b, a = butter(5, [low, high], btype="band")
+    return lfilter(b, a, y)
+def preprocess_signal(y, sr):
+    y = librosa.effects.preemphasis(y)
+    y = bandpass_filter(y, sr)
+    return y
 def extract_features(y, sr):
+    stft = np.abs(librosa.stft(y))
+    stft_db = librosa.amplitude_to_db(stft, ref=np.max)
     mel = librosa.feature.melspectrogram(
         y=y,
         sr=sr,
@@ -28,7 +41,22 @@ def extract_features(y, sr):
         sr=sr,
         n_mfcc=n_mels
     )
-    feature = np.stack([logmel, mfcc], axis=0)
+    chroma = librosa.feature.chroma_stft(
+        y=y,
+        sr=sr
+    )
+    target_freq = n_mels
+    target_time = logmel.shape[1]
+    mfcc = librosa.util.fix_length(mfcc, size=target_time, axis=1)
+    chroma = librosa.util.fix_length(chroma, size=target_time, axis=1)
+    stft_db = librosa.util.fix_length(stft_db, size=target_time, axis=1)
+    chroma = librosa.util.fix_length(chroma, size=target_freq, axis=0)
+    stft_db = stft_db[:target_freq, :]
+    mfcc = mfcc[:target_freq, :]
+    feature = np.stack(
+        [logmel, mfcc, chroma, stft_db],
+        axis=0
+    )
     return feature
 def pad_or_crop(spec, max_len=300):
     c, h, t = spec.shape
@@ -56,20 +84,32 @@ class LungSoundDataset(Dataset):
     def __getitem__(self, idx):
         path, label = self.samples[idx]
         y, sr = librosa.load(path, sr=sample_rate)
+        y = preprocess_signal(y, sr)
         feature = extract_features(y, sr)
         feature = torch.tensor(
             feature,
             dtype=torch.float32
         )
         feature = pad_or_crop(feature, max_len=300)
+
         label = torch.tensor(label, dtype=torch.long)
+
         return feature, label
+
 def compute_class_weights(dataset):
+
     labels = [label for _, label in dataset.samples]
+
     counts = Counter(labels)
+
     total = sum(counts.values())
+
     num_classes = len(CLASS_NAMES)
+
     weights = torch.zeros(num_classes)
+
     for cls_idx, count in counts.items():
+
         weights[cls_idx] = total / (num_classes * count)
+
     return weights
